195a27bd40df449aa7c7972ae0e34ff4828c519d
Monica <monica.riera@bts.tech>
2018-10-03 12:57:02 +0200

    Comparison Java - Kotlin

---
+++ src/main/java/tech/bts/javaReview/simpleProgram/Main.java
@@ -0,0 +1,77 @@
+package tech.bts.javaReview.simpleProgram;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello!");
+
+        //Declare variable
+        int age;
+        String name;
+
+        //Assign value
+        age = 15;
+
+        //Declare and assign
+        double weight = 40.5;
+
+        //if loop
+        if (age >= 18) {
+            System.out.println("adult");
+        } else if (age >= 13) {
+            System.out.println("teenager");
+        } else {
+            System.out.println("kid");
+        }
+
+        //for loop ("fori" shortcut)
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        //Create an empty list and add values
+        List<String> names = new ArrayList<>();
+        names.add("potato");
+        names.add("carrot");
+
+        //Create a list with some values
+        List<Integer> numbers = Arrays.asList(8, 4, 6, 5);
+        //Can't do numbers.add(3);
+
+        //Map
+        Map<String, Integer> ages = new HashMap<>();
+        ages.put("John", 30);
+        ages.put("Mary", 25);
+        System.out.println(ages.get("John"));
+
+        //Constant values
+        double price = 200;
+        double discount = 10;
+        final double finalPrice = price - discount;
+        //final will not allow us to modify the finalPrice in the future
+        //so we can't do this by mistake: finalPrice = price;
+        System.out.println("The final price is " + finalPrice);
+
+        Main.printHello("Monica");
+
+        System.out.println(power(2, 10));
+
+    }
+
+    /**Calculates the power of a given number*/
+    static double power (double base, double exponent) {
+        double result = 1;
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+        return result;
+    }
+
+    /**Prints hello*/
+    static void printHello (String name) {
+        System.out.println("Hello " + name + " :)");
+    }
+}
---
+++ src/main/java/tech/bts/javaReview/simpleProgram/MainKotlin.kt
@@ -0,0 +1,65 @@
+package tech.bts.javaReview.simpleProgram
+
+fun main(args: Array<String>) {
+
+    println("Hello, world!")
+
+    //Declare variables
+    var age : Int
+    var name : String
+
+    //Assign a value
+    age = 15;
+
+    //Declare and assign
+    var weight = 40.5 //It is not necessary to specify the type (Double) because it is inferred by the value
+
+
+    //if loop
+    if (age >= 18) {
+        println("adult")
+    } else if (age >= 13) {
+        println("teenager")
+    } else {
+        println("kid")
+    }
+
+    //for loop in range
+    for (i in 1..10) {
+        println(i)
+    }
+
+    //Create an empty list and add values
+    //We use Mutable(...) to be able to modify the list after it is defined
+    var names = mutableListOf<String>()
+    names.add("potato")
+    names.add("carrot")
+
+    //Create a list with some values
+    var numbers = listOf(8, 4, 6 ,5)
+    //Can't do numbers.add(3)
+
+    //Map
+    var ages = mutableMapOf<String, Int>()
+    ages.put("John", 30)
+    ages.put("Mary", 25)
+    println(ages.get("Mary"))
+
+    //Constant values
+    var price = 200.0
+    var discount = 10.0
+    val finalPrice = price - discount
+    //val will not allow us to modify the finalPrice in the future
+    //so we can't do this by mistake: finalPrice = price
+    println("The final price is $finalPrice")
+    println(power(2.0, 10))
+
+}
+
+fun power (base : Double, exponent : Int) : Double {
+    var result = 1.0
+    for (i in 1..exponent) {
+        result *= base
+    }
+    return result
+}
